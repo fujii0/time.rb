fdd34c71b576e7b31925ce28509e61c15d872a39
Hiroyuki FUJII <hfujiihs@gmail.com>
2016-08-16 12:23:14 +0900

    git config --global alias.graph

--- time.rb
+++ time.rb
@@ -8,3 +8,4 @@ end
 $stderr.puts filename
 open(filename,"w"){|file| for ii in lp..2*lp do file.puts ii end}
 $stderr.puts Time.now - start_time
+# git config --global alias.graph "log --graph --date-order --all --pretty=format:'%h%Cred%d %Cgreen%ad %Cblue%cn %Creset%s' --date=short"
